9c5dfe1fcd3d2c70efe89610923e002f9318aca0
Juan Pablo Posada Rodriguez <juan.posada@bootcamp-upgrade.com>
2022-12-05 20:00:19 +0100

    Iteration#1

---
+++ buscar-el-maximo.js
@@ -0,0 +1,13 @@
+
+function sum(numberOne , numberTwo) {
+    let masAlto;
+    if(numberOne>=numberTwo) {
+        return numberOne;
+    }
+    else {
+        return numberTwo;
+    }
+}
+
+let a = sum(10, 6);
+console.log(a);
